ec65092290d3e8ab80fcdadc4a357b377a245152
Satishvarma17 <satishvarmapakalapati17@gmail.com>
2025-12-30 12:54:16 +0530

    Update counter.js

--- 06-javascript/challenge-01/counter.js
+++ 06-javascript/challenge-01/counter.js
@@ -1,23 +1,26 @@
-let count=0;
-let mc=document.getElementById("mc");
-mc.innerText=count;
-function inc(){
-    count+=1;
-    mc.innerText=count;
-    mc.style.color='green'
-}
-function dec(){
-    if(count>0){
-        count-=1;
-        if(count==0){
-            mc.style.color='black'
-        }
-        mc.innerText=count;
+let count = 0;
+const mc = document.getElementById("mc");
+
+mc.textContent = `${count}`;
+
+const updateUI = () => {
+    mc.textContent = `${count}`;
+    mc.style.color = count > 0 ? "green" : "black";
+};
+
+const inc = () => {
+    count += 1;
+    updateUI();
+};
+
+const dec = () => {
+    if (count > 0) {
+        count -= 1;
+        updateUI();
     }
-    
-}
-function setc(){
-    count=0;
-    mc.innerText=count;
-    mc.style.color='black'
-}
+};
+
+const setc = () => {
+    count = 0;
+    updateUI();
+};
