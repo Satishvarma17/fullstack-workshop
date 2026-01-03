0efa36d82ddc14a61a40466897253546bfa7c634
satish <satishvarmapakalapati17@gmail.com>
2026-01-03 09:17:09 +0530

    add sql challenges

---
+++ 09-mysql/challenge-03/project-workload.sql
@@ -0,0 +1,7 @@
+select p.name as project_name,p.budget as budget,count(a.employee_id) as team_size,sum(a.hours_allocated) as total_hours 
+from projects as p 
+join assignments as a 
+on p.id=a.project_id 
+where budget>50000
+group by a.project_id
+order by total_hours desc;
