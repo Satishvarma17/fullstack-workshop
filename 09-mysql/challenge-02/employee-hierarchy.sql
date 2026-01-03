956ee50118e5d804eb2c867de29163b7ad27aa2f
Satishvarma17 <satishvarmapakalapati17@gmail.com>
2026-01-03 09:20:17 +0530

    added my-sql challenges

---
+++ 09-mysql/challenge-02/employee-hierarchy.sql
@@ -0,0 +1,4 @@
+select e.name as employee_name, m.name as manager_name 
+from employees as e 
+left join employees as m 
+on e.manager_id=m.id;
---
+++ 09-mysql/challenge-04/top-earners.sql
@@ -0,0 +1,3 @@
+select * from
+(select department, name, salary, dense_rank() over(partition by department order by salary desc) as  rank_in_dept from employees) as e 
+where rank_in_dept<=3;
