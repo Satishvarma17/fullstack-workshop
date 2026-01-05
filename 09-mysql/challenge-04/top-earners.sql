4dcd204c1f775a7ff47d4bb32b8da8193f3cc2f5
Satishvarma17 <satishvarmapakalapati17@gmail.com>
2026-01-05 09:26:16 +0530

    Update top-earners.sql

--- 09-mysql/challenge-04/top-earners.sql
+++ 09-mysql/challenge-04/top-earners.sql
@@ -1,3 +1,5 @@
-select * from
+select department, name, salary, rank_in_dept from
 (select department, name, salary, dense_rank() over(partition by department order by salary desc) as  rank_in_dept from employees) as e 
 where rank_in_dept<=3;
+
+--count(rank_in_dept)
